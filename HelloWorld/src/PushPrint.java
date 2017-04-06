cb547c52400346d04a39d1b0bc8250427bfe7434
hsdgpg <hsdgpg@naver.com>
2017-04-06 11:04:23 +0900

    Update PushPrint.java
    PushPrint.java modified on github

--- HelloWorld/src/PushPrint.java
+++ HelloWorld/src/PushPrint.java
@@ -2,7 +2,7 @@
 public class PushPrint {
 	public static void main(String[] args){
 		System.out.println("add java file");
-		System.out.println("Wish you where you are");
+		System.out.println("Wish you, where you are");
 	}
 
 }
